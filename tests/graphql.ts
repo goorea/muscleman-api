d9bd96bda783d33e531b76dbf83e6e46e253ecf4
jhs851 <jhs851@naver.com>
2021-09-22 16:57:06 +0900

    문이 열리네요2

--- tests/graphql.ts
+++ tests/graphql.ts
@@ -1,14 +1,31 @@
-import { graphql as _graphql } from 'graphql';
-import { ExecutionResult } from 'graphql/execution/execute';
-import { Maybe } from 'graphql/jsutils/Maybe';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { schema } from '@src/schema';
+import { SchemaLink } from '@apollo/client/link/schema';
+import { ApolloQueryResult } from '@apollo/client/core/types';
+import {
+  MutationOptions,
+  QueryOptions,
+} from '@apollo/client/core/watchQueryOptions';
+import { FetchResult } from '@apollo/client/link/core';
 
-export const graphql = async (
-  source: string,
-  variableValues?: Maybe<{ [key: string]: unknown }>,
-): Promise<ExecutionResult> =>
-  _graphql({
-    schema: await schema,
-    source,
-    variableValues,
+const client = async (token?: string) =>
+  new ApolloClient({
+    cache: new InMemoryCache(),
+    ssrMode: true,
+    link: new SchemaLink({ schema: await schema }),
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
   });
+
+export const query = async <T, TVariables>(
+  options: QueryOptions<TVariables, T>,
+  token?: string,
+): Promise<ApolloQueryResult<T>> =>
+  (await client(token)).query<T, TVariables>(options);
+
+export const mutate = async <TData, TVariables>(
+  options: MutationOptions<TData, TVariables>,
+  token?: string,
+): Promise<FetchResult<TData>> =>
+  (await client(token)).mutate<TData, TVariables>(options);
