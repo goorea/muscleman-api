0b1d1242be741fbce221975c73694dd3d53b5302
jhs851 <jhs851@naver.com>
2021-09-28 19:02:29 +0900

    [refactor] Validation Decorator 수정
    #51

--- src/resolvers/types/VerifyInput.ts
+++ src/resolvers/types/VerifyInput.ts
@@ -1,19 +1,15 @@
 import { Field, InputType } from 'type-graphql';
 import { User } from '@src/models/User';
-import { IsDefined, IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, MinLength } from 'class-validator';
 import { UserLimit } from '@src/limits/UserLimit';
 
 @InputType({ description: '사용자 이메일 인증 입력 객체' })
 export class VerifyInput implements Partial<User> {
   @Field(() => String, { description: '이메일' })
-  @IsDefined()
-  @IsString()
   @IsEmail()
   email: string;
 
   @Field(() => String, { description: '이메일 인증 토큰' })
-  @IsDefined()
-  @IsString()
   @MinLength(UserLimit.email_verify_token.minLength)
   email_verify_token: string;
 }
