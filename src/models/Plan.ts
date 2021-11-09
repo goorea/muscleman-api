e0f9802233156673a90e250911136b757fc56398
jhs851 <jhs851@naver.com>
2021-11-09 18:07:26 +0900

    [feat] Plan 모델에 최대 무게 토글하는 훅 추가
    #89

--- src/models/Plan.ts
+++ src/models/Plan.ts
@@ -4,6 +4,7 @@ import {
   getModelForClass,
   modelOptions,
   mongoose,
+  pre,
   prop,
   Ref,
   Severity,
@@ -17,7 +18,10 @@ import { UserQueryHelpers } from '@src/models/types/User';
 import AuthenticationError from '@src/errors/AuthenticationError';
 import { Role } from '@src/types/enums';
 import ForbiddenError from '@src/errors/ForbiddenError';
+import { toggleOneRM } from '@src/models/hooks/plan-hooks';
+import { WeightSet } from '@src/models/types/WeightSet';
 
+@pre<Plan>(['updateOne', 'findOneAndUpdate', 'updateMany'], toggleOneRM)
 @ObjectType({ implements: Model, description: '운동계획 모델' })
 @modelOptions({ options: { allowMixed: Severity.ALLOW } })
 export class Plan extends Model implements PlanMethods {
@@ -64,6 +68,29 @@ export class Plan extends Model implements PlanMethods {
     }
 
     return this;
+  }
+
+  hasWeightSets(
+    this: DocumentType<Plan, PlanQueryHelpers>,
+    sets: Plan['sets'],
+  ): sets is WeightSet[] {
+    return this.sets.every(
+      set =>
+        (set as WeightSet).weight !== undefined &&
+        (set as WeightSet).count !== undefined,
+    );
+  }
+
+  getOneRM(this: DocumentType<Plan, PlanQueryHelpers>): number {
+    if (this.hasWeightSets(this.sets)) {
+      const { weight, count } = this.sets
+        .sort((a, b) => b.weight - a.weight)
+        .slice(-1)[0];
+
+      return weight + weight * count * 0.025;
+    }
+
+    return 0;
   }
 }
 
--- src/models/types/Plan.ts
+++ src/models/types/Plan.ts
@@ -2,12 +2,20 @@ import { DocumentType } from '@typegoose/typegoose';
 import { Plan } from '@src/models/Plan';
 import { User } from '@src/models/User';
 import { UserQueryHelpers } from '@src/models/types/User';
+import { WeightSet } from '@src/models/types/WeightSet';
 
 export interface PlanMethods {
   checkPermission: (
     this: DocumentType<Plan, PlanQueryHelpers>,
     user: DocumentType<User, UserQueryHelpers>,
   ) => DocumentType<Plan>;
+
+  hasWeightSets: (
+    this: DocumentType<Plan, PlanQueryHelpers>,
+    sets: Plan['sets'],
+  ) => sets is WeightSet[];
+
+  getOneRM: (this: DocumentType<Plan, PlanQueryHelpers>) => number;
 }
 
 export interface PlanQueryHelpers {}
